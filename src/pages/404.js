f1f913bd43c9b36f1a1e668b4ea47c2172525b11
Luc Mosser <luc.mosser86@orange.fr>
2022-01-28 09:16:18 +0100

    modification of the 3D model

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,34 +1,34 @@
-import NextLink from 'next/link'
-import {
-  Box,
-  Heading,
-  Text,
-  Container,
-  Divider,
-  Button, 
-  useColorModeValue
-} from '@chakra-ui/react'
+// import NextLink from 'next/link'
+// import {
+//   Box,
+//   Heading,
+//   Text,
+//   Container,
+//   Divider,
+//   Button, 
+//   useColorModeValue
+// } from '@chakra-ui/react'
 
-const NotFound = () => {
-  return (
-    <Container>
-      <Heading as="h1">Not found</Heading>
-      <Text>The page you&apos;re looking for was not found.</Text>
-      <Divider my={6} />
-      <Box my={6} align="center">
-        <NextLink href="/">
-          <Button 
-            // rightIcon={<ChevronRightIcon />}
-            bgColor={useColorModeValue('#805ad5', '#fbd38d')} 
-            color={useColorModeValue('white', '#1a202c')} 
-            _hover={{ transform: 'scale(1.1)' }}
-          >
-          Return to home </Button>
-        </NextLink>
-      </Box>
-    </Container>
-  )
-}
+// const NotFound = () => {
+//   return (
+//     <Container>
+//       <Heading as="h1">Not found</Heading>
+//       <Text>The page you&apos;re looking for was not found.</Text>
+//       <Divider my={6} />
+//       <Box my={6} align="center">
+//         <NextLink href="/">
+//           <Button 
+//             // rightIcon={<ChevronRightIcon />}
+//             bgColor={useColorModeValue('#805ad5', '#fbd38d')} 
+//             color={useColorModeValue('white', '#1a202c')} 
+//             _hover={{ transform: 'scale(1.1)' }}
+//           >
+//           Return to home </Button>
+//         </NextLink>
+//       </Box>
+//     </Container>
+//   )
+// }
 
-export default NotFound
-export { getInitialProps } from '../components/chakra'
+// export default NotFound
+// export { getInitialProps } from '../components/chakra'
