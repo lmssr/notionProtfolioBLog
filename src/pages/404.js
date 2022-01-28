37cdb88d919534b78483030badf32c327e32753a
Luc Mosser <luc.mosser86@orange.fr>
2022-01-28 09:26:03 +0100

    modification of the 3D model

---
+++ src/pages/404.js
@@ -0,0 +1,34 @@
+import NextLink from 'next/link'
+import {
+  Box,
+  Heading,
+  Text,
+  Container,
+  Divider,
+  Button, 
+  useColorModeValue
+} from '@chakra-ui/react'
+
+const NotFound = () => {
+  return (
+    <Container>
+      <Heading as="h1">Not found</Heading>
+      <Text>The page you&apos;re looking for was not found.</Text>
+      <Divider my={6} />
+      <Box my={6} align="center">
+        <NextLink href="/">
+          <Button 
+            // rightIcon={<ChevronRightIcon />}
+            bgColor={useColorModeValue('#805ad5', '#fbd38d')} 
+            color={useColorModeValue('white', '#1a202c')} 
+            _hover={{ transform: 'scale(1.1)' }}
+          >
+          Return to home </Button>
+        </NextLink>
+      </Box>
+    </Container>
+  )
+}
+
+export default NotFound
+export { getInitialProps } from '../components/chakra'
