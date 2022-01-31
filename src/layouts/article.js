aa81606199b870ce2cba7ea84aef9f5f3c60b719
Luc Mosser <luc.mosser86@orange.fr>
2022-01-31 11:21:15 +0100

    adding a social image

--- src/layouts/article.js
+++ src/layouts/article.js
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion'
 import Head from 'next/head'
 import { GridItemStyle } from '@/components/grid-item'
+import { name, description, url, socialImage } from '@/lib/config';
 
 const variants = {
   hidden: { opacity: 0, x: 0, y: 20 },
@@ -23,6 +24,9 @@ const Layout = ({ children, title }) => (
           <title>{title} - Luc Mosser</title>
           <meta name="twitter:title" content={title} />
           <meta property="og:title" content={title} />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content={url} />
+          <meta property="og:image" content={socialImage} />
         </Head>
       )}
       {children}
